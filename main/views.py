5858f5bf12661c153fa8d47eaea503a86de28609
daniieelll <139460059+daniieelll@users.noreply.github.com>
2023-08-18 10:49:24 -0300

    Add files via upload
    sistema de login para 'donos', usando o método accounts.

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+
+from  django.contrib.auth.decorators import login_required
+
+
+@login_required
+def teste1(request):
+    return render(request,'main/testando.html')
